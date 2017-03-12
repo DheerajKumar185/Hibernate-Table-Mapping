96278c4c9d173899c138a80d1cf140863f1ce5e1
Dheeraj.Kumar <Dheeraj.Kumar@TSPL-LPT197.MSFTPNQ.com>
2017-03-12 14:53:21 +0530

    Updated with Many To Many Using 3 Table

---
+++ Hibernate_Table_Mapping/src/main/java/com/tech/doks/hibernate/many2many/using3table/Employee.java
@@ -0,0 +1,41 @@
+package com.tech.doks.hibernate.many2many.using3table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Employee {
+	@Id
+	private long empId;
+	private String name;
+	@OneToMany(mappedBy = "employee")
+	private List<Laptop> laptop = new ArrayList<>();
+
+	public long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(long empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Laptop> getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(List<Laptop> laptop) {
+		this.laptop = laptop;
+	}
+}
